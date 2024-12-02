092a6c7618849101e425a119779d181a4f73d6ad
hayley-d <hayleydod@proton.me>
2024-12-02 15:39:58 +0200

    day 2 complete

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -9,11 +9,15 @@ fn main() {
     let mut safe_count: usize = 0;
 
     for line in contents.lines() {
+        let mut error_count: usize = 0;
+
         let numbers: Vec<isize> = line
             .split_whitespace()
             .map(|n| n.parse::<isize>().unwrap())
             .collect();
-        if validate_difference(&numbers) && validate_order(&numbers) {
+        if validate_difference(&numbers, &mut error_count)
+            && validate_order(&numbers, &mut error_count)
+        {
             safe_count += 1;
         }
     }
@@ -22,29 +26,38 @@ fn main() {
 }
 
 /// Takes in a vector of numbers and validates that they follow the difference rules.
-fn validate_difference(level: &Vec<isize>) -> bool {
+fn validate_difference(level: &Vec<isize>, error_count: &mut usize) -> bool {
     for i in 1..level.len() {
         let diff: isize = level.get(i).unwrap() - level.get(i - 1).unwrap();
         if diff > 3 || diff < -3 || diff == 0 {
-            return false;
+            *error_count += 1;
+            if *error_count > 1 {
+                return false;
+            }
         }
     }
 
     return true;
 }
 
-fn validate_order(level: &Vec<isize>) -> bool {
+fn validate_order(level: &Vec<isize>, error_count: &mut usize) -> bool {
     let asc: bool = if level[0] > level[1] { false } else { true };
     for i in 1..level.len() {
         match asc {
             true => {
                 if level[i] < level[i - 1] {
-                    return false;
+                    *error_count += 1;
+                    if *error_count > 1 {
+                        return false;
+                    }
                 }
             }
             false => {
                 if level[i] > level[i - 1] {
-                    return false;
+                    *error_count += 1;
+                    if *error_count > 1 {
+                        return false;
+                    }
                 }
             }
         }
